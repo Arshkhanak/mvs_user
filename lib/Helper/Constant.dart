df210b7009231a280aa89aed9fcab5adc87960fd
Arsh Khan <arshkhan1.ak@gmail.com>
2022-03-26 15:48:54 +0530

    Initial commit

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,20 @@
+const String appName = 'MVS User';
+
+const String packageName = 'com.mvs.user';
+const String androidLink =
+    'https://play.google.user/store/apps/details?id=';
+
+const String iosPackage = 'com.mvs.user';
+const String iosLink = 'your ios link here';
+const String appStoreId = '123456789';
+
+const String deepLinkUrlPrefix = 'https://mvs.page.link';
+const String deepLinkName = 'MVS User';
+
+const int timeOut = 50;
+const int perPage = 10;
+
+const String baseUrl = 'https://mvs.ascithub.in/app/v1/api/';
+
+const String jwtKey = "ba13f7d76b380ad91e7717b186c32945a2b3e01a";
+
