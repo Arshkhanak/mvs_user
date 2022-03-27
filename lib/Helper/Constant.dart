54586fcc3e24ed623104acef7111f745a8bf3615
Arsh khan <57301016+Arshkhanak@users.noreply.github.com>
2022-03-27 12:24:15 +0530

    Update Constant.dart

--- lib/Helper/Constant.dart
+++ lib/Helper/Constant.dart
@@ -14,7 +14,7 @@ const String deepLinkName = 'MVS User';
 const int timeOut = 50;
 const int perPage = 10;
 
-const String baseUrl = "https://mvs.ascithub.in/app/v1/api/";
+const String baseUrl = 'https://mvs.ascithub.in/app/v1/api/';
 
 const String jwtKey = "334dc94e84bd10121dac8aa09dec46b36aeb9982";
 
